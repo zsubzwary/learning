a47e9fd0e06d8ada78939b6be24c2827114ae6af
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-04-06 18:41:23 +0500

    Reverted back

---
+++ public/component---src-pages-about-js-e6adfe86debff5847433.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{142:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(149);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},149:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-e6adfe86debff5847433.js.map
