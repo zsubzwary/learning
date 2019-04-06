c553ef5468d78551c4f060b2abea221f11216179
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-04-06 18:33:56 +0500

    Changed Site

--- public/component---src-pages-about-js-48129f01be8b81f0808b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{141:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(146);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},146:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
-//# sourceMappingURL=component---src-pages-about-js-48129f01be8b81f0808b.js.map
